7300ece35b948db2e93b18c9783a3037317c19d1
guiernew <gaudstoo@gmail.com>
2016-07-04 23:30:18 -0300

    Resto do app

---
+++ app/src/test/java/speak/guiernew/com/github/speak/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package speak.guiernew.com.github.speak;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
